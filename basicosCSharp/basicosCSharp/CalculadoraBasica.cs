63728a604d289934245a7f0df7ea8a7e871dd92a
GerardoAc <gera.araya3@gmail.com>
2019-11-30 10:59:13 -0600

    se crea objeto simple en c#

---
+++ basicosCSharp/basicosCSharp/CalculadoraBasica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basicosCSharp
+{
+    class CalculadoraBasica
+    {
+        private double ultimoResultado;
+        public double Sumar(double primerValor, double segundoValor) {
+            ultimoResultado = primerValor + segundoValor;
+            return ultimoResultado;
+        }
+        public double Dividir(double primerValor, double segundoValor) {
+            ultimoResultado = primerValor / segundoValor;
+            return ultimoResultado;
+        }
+    }
+}
