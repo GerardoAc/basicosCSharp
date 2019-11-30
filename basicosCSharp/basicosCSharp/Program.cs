353861264eeaa7e2fd668ef68b180d3e452b2e9c
GerardoAc <gera.araya3@gmail.com>
2019-11-30 10:07:45 -0600

    se trabaja con while y do while

--- basicosCSharp/basicosCSharp/Program.cs
+++ basicosCSharp/basicosCSharp/Program.cs
@@ -58,11 +58,19 @@ namespace basicosCSharp
                 Console.WriteLine("Este minuto es impar");
             }
 
+            int rondasFaltantes = 3;
+            while (rondasFaltantes > 0)
+            {
+                rondasFaltantes--;
+            }
+            rondasFaltantes = 3;
+            do
+            {
+                rondasFaltantes--;
+            } while (rondasFaltantes > 0);
 
-
-
-            Console.ReadLine();
         }
         
     }
+
 }
